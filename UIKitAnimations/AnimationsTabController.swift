bde19bc28e7f834eefd7e288ea2158249b9c9b64
maitreebain <maitreebain@gmail.com>
2020-01-31 10:53:29 -0500

    set up tab bar and tab bar items

--- UIKitAnimations/AnimationsTabController.swift
+++ UIKitAnimations/AnimationsTabController.swift
@@ -9,15 +9,27 @@
 import UIKit
 
 class AnimationsTabController: UITabBarController {
-
+    
+    private lazy var sampleAnimationsVC: SampleAnimationsController = {
+        let viewController = SampleAnimationsController()
+        viewController.tabBarItem = UITabBarItem(title: "Sample Animations", image: UIImage(systemName: "1.circle"), tag: 0)
+        return viewController
+    }()
+    
+    private lazy var constraintsAnimationsVC: ConstraintsAnimationsController = {
+        let viewController = ConstraintsAnimationsController()
+        viewController.tabBarItem = UITabBarItem(title: "Constraints Animations", image: UIImage(systemName: "2.circle"), tag: 1)
+        return viewController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         //set View Controllers for the tab bar
         // in our app we will have two tabs
         
-        viewControllers = [SampleAnimationsController(), ConstraintsAnimationsController()]
+        viewControllers = [sampleAnimationsVC, constraintsAnimationsVC]
     }
-
+    
     
 }
